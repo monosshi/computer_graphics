ff052869c77d8f9087738f795e37769aacd12878
monosshi <monosshi.cs@gmail.com>
2026-01-23 22:07:33 +0600

    car shape

---
+++ c.cpp
@@ -0,0 +1,19 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    // Car body
+    rectangle(100, 200, 400, 260);
+    rectangle(150, 160, 350, 200);
+
+    // Wheels
+    circle(160, 260, 20);
+    circle(340, 260, 20);
+
+    getch();
+    closegraph();
+    return 0;
+}

b7a0736a1d8dc921e34e5731934a2a4310ff8cdf
monosshi <monosshi.cs@gmail.com>
2026-02-24 23:34:24 +0600

    midpoint circle

---
+++ MidpointCircle.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+     outtextxy(200, 10, "Name");
+     outtextxy(200, 30, "ID:");
+
+    int xc = 250, yc = 250, r = 80;
+    int x = 0, y = r;
+    int d = 1-r;
+
+    while (x <= y)
+    {
+        putpixel(xc + x, yc + y, WHITE);
+        putpixel(xc - x, yc + y, WHITE);
+        putpixel(xc + x, yc - y, WHITE);
+        putpixel(xc - x, yc - y, WHITE);
+        putpixel(xc + y, yc + x, WHITE);
+        putpixel(xc - y, yc + x, WHITE);
+        putpixel(xc + y, yc - x, WHITE);
+        putpixel(xc - y, yc - x, WHITE);
+
+        if (d < 0)
+            d = d + 2 * x + 3;
+        else {
+            d = d + 2 * (x - y) + 5;
+            y--;
+        }
+        x++;
+    }
+
+    getch();
+    closegraph();
+    return 0;
+}
+
